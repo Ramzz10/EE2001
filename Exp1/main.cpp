691b7e011a18bb6cf48b73619c20856ed3d711f2
Aswinkumar <aswinkumar1999@gmail.com>
2019-01-22 00:59:20 +0530

    Created generate.cpp to general truth table inputs. and stuff works :P

--- Exp1/main.cpp
+++ Exp1/main.cpp
@@ -12,29 +12,39 @@ int main(){
   short int a,b,c,d,out;
   //Circuit 1
   cout<<"A"<<"\t||"<<"B"<<"\t||"<<"C"<<"\t||"<<"C1 Structual"<<"\t||"<<"C1 Behavioural";
+  cout<<"\n";
   while(data_input>> a >> b >> c ){
       C1_str(&out,a,b,c);
-      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<out<<"\t||";
+      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<out<<"\t\t||";
       C1_beh(&out,a,b,c);
       cout<<out;
+      cout<<"\n";
   }
+  data_input.close();
   cout<<"\n\n";
+  ifstream data_input1("data.dat");
   //Circuit 2
   cout<<"B"<<"\t||"<<"C"<<"\t||"<<"D"<<"\t||"<<"C2 Structual"<<"\t||"<<"C2 Behavioural";
-  while(data_input>> a >> b >> c ){
+  cout<<"\n";
+  while(data_input1>> a >> b >> c ){
       C2_str(&out,a,b,c);
-      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<out<<"\t||";
+      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<out<<"\t\t||";
       C2_beh(&out,a,b,c);
       cout<<out;
+      cout<<"\n";
   }
   cout<<"\n\n";
+  data_input1.close();
+  ifstream data_input2("data4.dat");
   //Circuit 3
   cout<<"A"<<"\t||"<<"B"<<"\t||"<<"C"<<"\t||"<<"D"<<"\t||"<<"C3 Structual"<<"\t||"<<"C3 Behavioural";
-  while(data_input>> a >> b >> c >> d ){
+  cout<<"\n";
+  while(data_input2>> a >> b >> c >> d ){
       C3_str(&out,a,b,c,d);
-      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<d<<"\t||"<<out<<"\t||";
+      cout<<a<<"\t||"<<b<<"\t||"<<c<<"\t||"<<d<<"\t||"<<out<<"\t\t||";
       C3_beh(&out,a,b,c,d);
       cout<<out;
+      cout<<"\n";
   }
   return 0;
 }
