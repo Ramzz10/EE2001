8acd588ecc7d7f8ef488b931940aa5729e7db006
Ramzz <ramzz@gmail.com>
2019-02-04 21:54:40 +0530

    Added Subtracter and Demux for findind max.

--- Exp11/T4/mod.v
+++ Exp11/T4/mod.v
@@ -1,19 +1,17 @@
 module max(a,b,out);
 input [3:0] a,b;
-reg [3:0] wi;
-wire [3:0] sum,sum2,tmp;
 output [3:0] out;
-outpit cout;
-s=1'b0;
+reg [3:0] wi;
+output cout;
+sub s1(a,b,cout);
 
-comp s0(s,b[0],c[0]);
-comp s1(s,b[1],c[1]);
-comp s2(s,b[2],c[2]);
-comp s3(s,b[3],c[3]);
-assign tmp = {s&~s,s&~s,s&~s,~s};
-four_bit fb1(c,tmp,sum,cout);
-four_bit fb2(sum,a,sum2,cout);
-
+always@(a or b or out or cout)
+begin
+if(cout==0)
+  wi=a;
+else if(cout==1)
+  wi=b;
+end
 
 assign out = wi;
 
--- Exp11/T4/mod2.v
+++ Exp11/T4/mod2.v
@@ -1,3 +1,21 @@
+module sub(a,b,hell);
+
+input [3:0] a,b,c;
+reg [3:0] wi;
+wire [3:0] sum,sum2,tmp;
+output cout,hell;
+wire s;
+assign s=1'b0;
+comp s0(s,b[0],c[0]);
+comp s1(s,b[1],c[1]);
+comp s2(s,b[2],c[2]);
+comp s3(s,b[3],c[3]);
+assign tmp = {s&~s,s&~s,s&~s,~s};
+four_bit fb1(c,tmp,sum,cout);
+four_bit fb2(sum,a,sum2,cout);
+assign hell = sum2[3];
+endmodule
+
 module four_bit(a,b,s,cout);
 
 input [3:0] a,b;
