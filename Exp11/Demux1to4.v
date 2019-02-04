1630284fbaede3a78aafd7b4692b319e608f7961
Ramzz <ramzz@gmail.com>
2019-02-04 16:14:58 +0530

    finished almost.....:)

--- Exp11/Demux1to4.v
+++ Exp11/Demux1to4.v
@@ -1,34 +1,32 @@
 module demux1_beh(I,s,out);
 input I;
 input [1:0] s;
-reg [3:0] wi;
 output [3:0] out;
-assign wi[0]=0;
+reg [3:0] out;
 
-always@(I or s or wi)
+always@(I or s or out)
 begin
 if(s==0)
-  wi[0]=I;
+  out=4'b0001;
 else if(s==1)
-  wi[1]=I;
+  out=4'b0010;
 else if(s==2)
-  wi[2]=I;
+  out=4'b0100;
 else if(s==3)
-  wi[3]=I;
+  out=4'b1000;
 end
 
-assign out = wi;
 
 endmodule
 
-//module demux4_dat(I,s,out);
-//input [3:0]I;
-//input [1:0]s;
-//output out;
+module demux1_dat(I,s,out);
+input I;
+input [1:0] s;
+output [3:0] out;
 
-//assign out = (s==0)?I[0]:
-            // (s==1)?I[1]:
-            // (s==2)?I[2]:
-             //(s==3)?I[3]:1'bx;
+assign out = (s==0)?4'b0001:
+             (s==1)?4'b0010:
+             (s==2)?4'b0100:
+             (s==3)?4'b1000:4'b0000;
 
-//endmodule
+endmodule
--- Exp11/testbench_demux.v
+++ Exp11/testbench_demux.v
@@ -4,11 +4,12 @@ reg [1:0]s;
 output [3:0]out1,out2;
 demux1_beh DUT(I,s,out1);
 
+demux1_dat DUT1(I,s,out2);
 initial
 begin
 $dumpfile("test1.vcd");
 $dumpvars(0,test_demux);
-$monitor("At time = %t,I = %b,s = %b,out_beh = %b" ,$time,I,s,out1);
+$monitor("At time = %t,I = %b,s = %b,out_beh = %b,out_dat = %b" ,$time,I,s,out1,out2);
 I=1; s=2'b00;                    //t=0
 #10 I=1; s=2'b10;     //t=10
 #10 I=1; s=2'b01;      //t=20
