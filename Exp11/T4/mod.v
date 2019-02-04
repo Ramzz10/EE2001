78e63e6221ded57558fe67f13e0d345ba2dd63d2
Ramzz <ramzz@gmail.com>
2019-02-04 21:13:10 +0530

    done something v2

--- Exp11/T4/mod.v
+++ Exp11/T4/mod.v
@@ -1,14 +1,19 @@
 module max(a,b,out);
 input [3:0] a,b;
 reg [3:0] wi;
+wire [3:0] sum,sum2,tmp;
 output [3:0] out;
-always@(a or b or wi)
-begin
-if(a>b)
-  wi=a;
-else if (b>a)
-  wi=b;
-end
+outpit cout;
+s=1'b0;
+
+comp s0(s,b[0],c[0]);
+comp s1(s,b[1],c[1]);
+comp s2(s,b[2],c[2]);
+comp s3(s,b[3],c[3]);
+assign tmp = {s&~s,s&~s,s&~s,~s};
+four_bit fb1(c,tmp,sum,cout);
+four_bit fb2(sum,a,sum2,cout);
+
 
 assign out = wi;
 
